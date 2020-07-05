304e835b8aa28af20a7aac6cb58510f413830840
Shruti Rao <48805685+raoshruti16@users.noreply.github.com>
2020-07-06 01:24:42 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  6 01:07:42 2020
+
+@author: shruti
+"""
+
+
+EMAIL_ADDRESS = "youraccount@gmail.com"
+PASSWORD = "password*"
+SEND_TO = "reciever_email"
